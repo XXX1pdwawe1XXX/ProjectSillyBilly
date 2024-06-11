0dc256db9d2500f9cc8b4308cb3d381538fb3f42
RobbyInfinger <rsinfin@clemson.edu>
2024-06-11 11:43:24 -0400

    Documentation

--- Sail.cs
+++ Sail.cs
@@ -3,6 +3,9 @@ using System.Numerics;
 
 namespace SillyBilly.SillyBilly
 {
+    /*
+        The following class handles math and operations relating to sails
+    */
     class Sail
     {
         private bool headWind = false;
@@ -20,23 +23,47 @@ namespace SillyBilly.SillyBilly
             UpdateWindDirection();
         }
 
-        public int CalculateWindSailThrust(Vector3 windDirection, Vector3 sailDirection)
+        /*
+            The following method will calculate an integer thrust value dependent on the difference in angle between
+            2 vectors. While the vectors are 3D, they'll functionally be 2D.
+
+            @pre Vector must be a valid vector with valid float datatypes
+            @post theta = arccos([windDirection * sailDirection]/[||windDirection||sailDirection||]) * 180/pi
+                  windScale * cos(theta * pi/180)
+            @return [uint >= 0 <= MAX_INT]
+        */
+        public uint CalculateWindSailThrust(Vector3 windDirection, Vector3 sailDirection)
         {
             float dotProduct = Vector3.Dot(windDirection, sailDirection);
             float angle = Math.Acos(dotProduct / (windDirection.Length() * sailDirection.Length())) * (180 / Math.PI);
             return (int)(windScale * Math.Cos(angle * (Math.PI / 180)));
         }
 
+        /*
+            The following method will create a new random wind vector
+
+            @pre n/a
+            @post float rotational Vector3
+            @return n/a
+        */
         public void UpdateWindDirection()
         {
             float x = (float)(rand.NextDouble() * 2 - 1);
             float y = (float)(rand.NextDouble() * 2 - 1);
-            float z = (float)(rand.NextDouble() * 2 - 1);
+            //float z = (float)(rand.NextDouble() * 2 - 1);
+            float z = 0;
 
             windDirection = Vector3.Normalize(new Vector3(x, y, z));
             lastWindUpdate = DateTime.Now;
         }
 
+        /*
+            The following method will update the wind direction every 5-10 minutes.
+
+            @pre n/a
+            @post new windDirection
+            @return n/a
+        */
         public void CheckAndUpdateWindDirection()
         {
             TimeSpan elapsed = DateTime.Now - lastWindUpdate;
@@ -47,10 +74,30 @@ namespace SillyBilly.SillyBilly
             }
         }
 
+        /*
+            Getter for Wind Direction...
+
+            @pre n/a
+            @post checks current windDirection
+            @return Wind Direction 
+        */
         public Vector3 GetWindDirection()
         {
             CheckAndUpdateWindDirection();
             return windDirection;
         }
+        
+        /*
+            The following calculates the sail area of a sail using the "Arrestor Hook" method
+
+            @pre Unsure
+            @post Calculates the area of a 2D Sail
+            @return Wind Direction 
+        */
+        /*
+        public float calculateSailArea(//Something){
+
+        }
+        */
     }
 }
---
+++ com.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Numerics;
+using SillyBilly.SillyBilly;
+// All Vrage classes etc...
+
+namespace Comm.Comm
+{
+    /*
+        The following class will handle communication with SE. 
+    */
+    class Comm
+    {
+        /*
+            The following method will update the thrust value in SE most likley using the 
+            Thruster type. [Implementations may vary depending on block type used]
+            
+            @pre n/a
+            @post [uint >= 0 <= MAX_INT(Newtons)]
+            @return n/a
+        */
+        public void updateThrust(){
+
+        }
+        
+        
+    }   
+}
